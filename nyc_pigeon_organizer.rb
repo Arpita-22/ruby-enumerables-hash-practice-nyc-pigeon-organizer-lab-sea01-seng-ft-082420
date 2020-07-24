e6adc1ca35ccb7683426ccb401fb4e6092d8a009
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-24 18:51:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({})do |(key,value),final_hash|
+    value.each do |inner_key,inner_value|
    binding.pry 
+ end
  end
 end
