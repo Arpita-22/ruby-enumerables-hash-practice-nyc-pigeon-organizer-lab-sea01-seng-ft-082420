17813d19b149580a4898f5a01e0603820609e59b
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-24 19:15:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  final_hash = {}
   data.each_with_object({})do |(key,value),final_hash|
     value.each do |inner_key,inner_value|
       inner_value.each do |name|
